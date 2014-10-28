e87c5f23cab1aa36cc4134c12481ee77edd3db3b
Shane Hender <shender@zendesk.com>
2014-10-28 15:04:36 +0000

    Hooked ticket save

--- public/app.js
+++ public/app.js
@@ -1,20 +1,29 @@
 (function() {
 
   return {
+
+    requests: {
+      fetchBookmarks: {
+        url: '/api/v2/bookmarks.json',
+        type: 'GET'
+      },
+    },
+
     events: {
       'app.activated':'showLeaderboard',
+      'ticket.status.changed' : 'ticketStatusHookHandler',
       'ticket.save' : 'saveHookHandler',
-      'ticket.status.changed' : 'ticketStatusHookHandler'
     },
 
     showLeaderboard: function(data) {
-      console.log("ticket: " + data);
+      console.log("Loading app: " + this.ticket());
       this.switchTo('zen-force');
     },
 
     saveHookHandler: function(data) {
-      console.log("Saved a ticket: " + this.ticket().id());
+      console.log("User: " + this.currentUser().email() + " Saved a ticket: " + this.ticket().id());
       console.log("status " + this.ticket().status());
+      console.log("priority " + this.ticket().priority());
       console.log("by " + this.ticket().assignee().user().email());
     },
 
