a10792b47a5f42c9bb5c4526370ab4946246757c
Shane Hender <shender@zendesk.com>
2014-10-28 18:33:40 +0000

    refactor

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@
       fetchUserDetails: function (args) {
         return {
           url: 'https://stormy-crag-5102.herokuapp.com/users/' + args.id + '/details.json',
-          contentType: 'application/json',
+          dataType: 'json',
           data: args,
           type: 'GET'
         }
@@ -15,14 +15,14 @@
       fetchLeaderboard: function () {
         return {
           url: 'https://stormy-crag-5102.herokuapp.com/leaderboard.json',
-          contentType: 'application/json',
+          dataType: 'json',
           type: 'GET'
         }
       },
       addUserPoints: function (args) {
         return {
           url: 'https://stormy-crag-5102.herokuapp.com/users/' + args.id + '/allocate_points.json',
-          contentType: 'application/json',
+          dataType: 'json',
           data: args,
           type: 'POST'
         }
@@ -46,20 +46,27 @@
       this.ajax('fetchUserDetails', requestParams).done(function(details) {
         console.log("Got the user details: " + JSON.stringify(details) + "... getting leaderboard!");
 
-        this.ajax('fetchLeaderboard').done(function(leaderData) {
-          console.log("Got the leaderboard: " + JSON.stringify(leaderData));
-          details.leaderboard = leaderData.leaderboard;
-          this.switchTo('zen-force', details);
-        });
+        this.ajax('fetchLeaderboard')
+          .done(function(leaderData) {
+            console.log("Got the leaderboard: " + JSON.stringify(leaderData));
+            details.leaderboard = leaderData.leaderboard;
+            this.switchTo('zen-force', details);
+          }).error(function () {
+            console.log("Failed getting leaderboard!");
+            this.switchTo('zen-force', details);
+          });
       });
     },
 
     saveHookHandler: function() {
       if (this.ticket().status() == "solved") {
-        requestParams.name = this.currentUser().name();
-        requestParams.email = this.currentUser().email();
-        requestParams.id = this.currentUser().id();
-        requestParams.external_id = this.currentUser().id();
+        var requestParams = {
+          name:  this.currentUser().name(),
+          email: this.currentUser().email(),
+          id:    this.currentUser().id(),
+          external_id: this.currentUser().id(),
+          points: this.ticket().priority()
+        };
         console.log("Send ticket solving to backend: " + JSON.stringify(requestParams));
         this.ajax('addUserPoints', requestParams).done(function () {
           this.showLeaderboard();
