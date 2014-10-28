1dc723afa401c21c872cfb3ba63bf21bfec229c3
Shane Hender <shender@zendesk.com>
2014-10-28 14:38:44 +0000

    baseline app created

--- public/app.js
+++ public/app.js
@@ -2,11 +2,26 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'showLeaderboard',
+      'ticket.save' : 'saveHookHandler',
+      'ticket.status.changed' : 'ticketStatusHookHandler'
     },
 
-    doSomething: function() {
-    }
+    showLeaderboard: function(data) {
+      console.log("ticket: " + data);
+      this.switchTo('zen-force');
+    },
+
+    saveHookHandler: function(data) {
+      console.log("Saved a ticket: " + this.ticket().id());
+      console.log("status " + this.ticket().status());
+      console.log("by " + this.ticket().assignee().user().email());
+    },
+
+    ticketStatusHookHandler: function(data) {
+      console.log("Ticket status change: " + this.ticket().id());
+    },
+
   };
 
 }());
